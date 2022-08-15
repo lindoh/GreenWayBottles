181c19726ed16b2a2400021a3fb09f2d3e09dbe5
Lindo Gamede <lindohgamede@gmail.com>
2022-08-15 23:20:07 +0200

    Added SaveData method in DatabaseServices class

--- GreenWayBottles/Services/DatabaseService.cs
+++ GreenWayBottles/Services/DatabaseService.cs
@@ -1,7 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data;
 using Microsoft.Data.SqlClient;
+using GreenWayBottles.Models;
 
 namespace GreenWayBottles.Services
 {
@@ -10,8 +9,58 @@ namespace GreenWayBottles.Services
         SqlConnection sqlConnection;
         SqlCommand sqlCommand;
 
+        public DatabaseService()
+        {
+            string DataConnection = "data source = localhost; integrated security = true; initial catalog=GreenWayData";
+            sqlConnection = new SqlConnection(DataConnection);
+            sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+        }
 
+        public bool SaveData(Users user)
+        {
+            bool isSaved = false;
 
+            try
+            {
+                sqlCommand.Parameters.Clear();
+                sqlCommand.CommandText = "AddCollector";
+
+                sqlCommand.Parameters.AddWithValue("@FirstName", user.FirstName);
+                sqlCommand.Parameters.AddWithValue("@LastName", user.LastName);
+                sqlCommand.Parameters.AddWithValue("@IdNumber", user.IdNumber);
+                sqlCommand.Parameters.AddWithValue("@Gender", user.Gender);
+                sqlCommand.Parameters.AddWithValue("@HighestQlfn", user.HighestQlfn); ;
+                sqlCommand.Parameters.AddWithValue("@IncomeRange", user.IncomeRange);
+                sqlCommand.Parameters.AddWithValue("@Email", user.Email);
+                sqlCommand.Parameters.AddWithValue("@CellNumber", user.CellNumber);
+                sqlCommand.Parameters.AddWithValue("@StreetAddress", user.StreetAddress);
+                sqlCommand.Parameters.AddWithValue("@Suburb", user.Suburb);
+                sqlCommand.Parameters.AddWithValue("@City", user.City);
+                sqlCommand.Parameters.AddWithValue("@Province", user.Province);
+                sqlCommand.Parameters.AddWithValue("@Country", user.Country);
+
+                //Open Sql database connection
+                sqlConnection.Open();
+
+                //If affected number of rows is > 0, then data is saved successfully
+                int NoOfRowsAffected = sqlCommand.ExecuteNonQuery();
+                isSaved = NoOfRowsAffected > 0;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            
+
+            return isSaved;
+        }
 
     }
 }
